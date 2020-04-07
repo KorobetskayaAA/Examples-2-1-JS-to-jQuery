5b780f3a130010bf4605fd55509875685dc8b963
А.Коробецкая <kornast@yandex.ru>
2020-04-07 15:40:20 +0400

    Collapse на jQuery

--- js/collapse.js
+++ js/collapse.js
@@ -1,16 +1,7 @@
-window.addEventListener("load", function () {
-
-	let collapses = document.getElementsByClassName('collapse');
-
-	for (let i = 0; i < collapses.length; i++) {
-		let header = collapses[i].getElementsByTagName("h3")[0];
-		let article = collapses[i].getElementsByTagName("article")[0];
-
-		article.classList.add("hidden");
-
-		header.addEventListener('click', function() {
-			header.classList.toggle("expanded");
-			article.classList.toggle("hidden");
-		});
-	}
+$(document).ready(function() {
+	$(".collapse>article").hide();
+	$(".collapse>h3").click(function() {
+		$(this).toggleClass("expanded");
+		$(this).next("article").slideToggle(500);
+	});
 });
