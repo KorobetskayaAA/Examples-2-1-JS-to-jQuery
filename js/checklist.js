91e642675b6b83566bf0aa73ed64e5f632287596
А. Коробецкая <kornast@yandex.ru>
2020-04-21 15:16:00 +0400

    Кнопки теперь генерируются
    Добавлен второй список

--- js/checklist.js
+++ js/checklist.js
@@ -4,19 +4,31 @@ window.addEventListener('load', function() {
 	for (let i = 0; i < checklists.length; i++) {
 		let checkboxes = checklists[i].querySelectorAll("input[type='checkbox']");
 
-		let buttonCheckAll = checklists[i].querySelector('button[name="checkAll"]');
+		let buttonCheckAll = document.createElement("button");
+		buttonCheckAll.name = "checkAll";
+		buttonCheckAll.innerText = "отметить все";	
 		buttonCheckAll.addEventListener('click', () => {
 			checkboxes.forEach((checkbox) => checkbox.checked = true);
 		});
 
-		let buttonCheckNone = checklists[i].querySelector('button[name="checkNone"]');
+		let buttonCheckNone = document.createElement("button");
+		buttonCheckNone.name = "checkNone";
+		buttonCheckNone.innerText = "снять все";
 		buttonCheckNone.addEventListener('click', () => {
 			checkboxes.forEach((checkbox) => checkbox.checked = false);
 		});
 
-		let buttonCheckToggle = checklists[i].querySelector('button[name="checkToggle"]');
+		let buttonCheckToggle = document.createElement("button");
+		buttonCheckToggle.name = "checkToggle";
+		buttonCheckToggle.innerText = "перевернуть";
 		buttonCheckToggle.addEventListener('click', () => {
 			checkboxes.forEach((checkbox) => checkbox.checked = !checkbox.checked);
 		});
+
+		let panelCheck = document.createElement("div");
+		panelCheck.classList.add("panel");
+		panelCheck.append(buttonCheckAll, buttonCheckNone, buttonCheckToggle);
+
+		checklists[i].prepend(panelCheck);
 	}
 });
