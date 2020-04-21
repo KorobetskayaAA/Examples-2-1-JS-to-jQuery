9949536082aad3d9ad91d61be0688040229defd5
А. Коробецкая <kornast@yandex.ru>
2020-04-21 14:32:37 +0400

    Добавлен счетчик для появления ссылки

--- js/timedlink.js
+++ js/timedlink.js
@@ -1,3 +1,18 @@
 window.addEventListener('load', function() {
+	let time = 15;
+	let linkBox = document.getElementById("linkBox");
+	let timeNode = linkBox.getElementsByClassName("timer")[0];
 
+	updateTimer();
+
+	function updateTimer() {
+		if (time >= 0) {
+			timeNode.innerText = time;
+			setTimeout(updateTimer, 1000);
+		}
+		else {
+			linkBox.innerHTML = '<a href="index.html">Ссылка для скачивания файла</a>';
+		}
+		time--;
+	};
 });
